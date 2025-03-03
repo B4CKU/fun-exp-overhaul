752017a458bc0c65467f1ddc7d9edb82bccea1bd
B4CKU <szymekstrojny@gmail.com>
2025-03-03 18:40:32 +0100

    init

---
+++ src/client/java/com/zhaba/funexpoverhaul/FunExperienceOverhaulClient.java
@@ -0,0 +1,10 @@
+package com.zhaba.funexpoverhaul;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class FunExperienceOverhaulClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
